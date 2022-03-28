3d91c5a5cce2eb284a0dfb2d4dac3ea29beabf93
Susana Ung <susana.ung@hotmail.com>
2022-03-27 20:00:16 -0400

    Change useStatistics() to return empty stats when no workouts are passed

--- workout-app/src/hooks/useStatistics.js
+++ workout-app/src/hooks/useStatistics.js
@@ -5,23 +5,26 @@ export const useStatistics = (workouts) => {
     const [statistics, setStatistics] = useState({})
 
     useEffect(() => {
-        const totalTime = workouts.reduce((total, next) => total + next.totalTime, 0);
-        const totalDistance = workouts.reduce((total, next) => parseFloat(total + next.distance), 0);
-        const averageDistance = totalDistance / workouts.length;
-        const avgSpeed = workouts.reduce((total, next) => parseFloat(total) + parseFloat(next.avgSpeed), 0) / workouts.length;
-        const maxAlt = workouts.reduce((a, b) => a.maxAlt > b.maxAlt ? a.maxAlt : b.maxAlt, 0);
-        const calsBurned = workouts.reduce((total, next) => total + next.calsBurned, 0);
-        const startTime = workouts.reduce((total, next) => total + next.startTime, 0);
-
-        const stat = new Statistics({
-            totalDistance: totalDistance.toFixed(2),
-            avgDistance: averageDistance,
-            totalTime: totalTime,
-            avgSpeed: avgSpeed.toFixed(2),
-            maxAlt: maxAlt,
-            calsBurned: calsBurned,
-            startTime : startTime
-        })
+        let stat = new Statistics({});
+        if (workouts.length !== 0) {
+            const totalTime = workouts.reduce((total, next) => total + next.totalTime, 0);
+            const totalDistance = workouts.reduce((total, next) => parseFloat(total + next.distance), 0);
+            const averageDistance = totalDistance / workouts.length;
+            const avgSpeed = workouts.reduce((total, next) => parseFloat(total) + parseFloat(next.avgSpeed), 0) / workouts.length;
+            const maxAlt = workouts.reduce((a, b) => a.maxAlt > b.maxAlt ? a.maxAlt : b.maxAlt, 0);
+            const calsBurned = workouts.reduce((total, next) => total + next.calsBurned, 0);
+            const startTime = workouts.reduce((total, next) => total + next.startTime, 0);
+            
+            stat = new Statistics({
+                totalDistance: totalDistance.toFixed(2),
+                avgDistance: averageDistance,
+                totalTime: totalTime,
+                avgSpeed: avgSpeed.toFixed(2),
+                maxAlt: maxAlt,
+                calsBurned: calsBurned,
+                startTime : startTime
+            })
+        }
         setStatistics(stat);
       }, [workouts]);
     
